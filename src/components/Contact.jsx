f17aabf1fdb22165701842b40c052069afc027a5
Ricardo <javi26028@outlook.com>
2023-07-09 16:39:40 -0600

    Se repara el formulario, se sigue usando netlify

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,95 +1,68 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react'
 
 const Contact = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-[1200px] p-2 mx-auto"
-      netlify
-      data-netlify="true"
-    >
-      <div className="mb-4">
-        <label htmlFor="nombre" className="block text-text">
-          Nombre
-        </label>
-        <input
-          type="text"
-          id="nombre"
-          {...register('nombre', { required: true })}
-          className={`w-full px-3 py-2 border rounded-md ${errors.nombre ? 'border-red-500' : 'border-gray-300'
-            }`}
-        />
-        {errors.nombre && (
-          <span className="text-red-500">Este campo es requerido</span>
-        )}
+    <form name="contact" method="POST" netlify>
+      <div className="bg-dark p-4 rounded-lg">
+        <h2 className="text-white text-lg mb-4">Formulario de contacto</h2>
+        <div className="mb-4">
+          <label className="block text-white mb-1" htmlFor="nombre">
+            Nombre
+          </label>
+          <input
+            className="w-full px-3 py-2 rounded-md bg-darkSecundary text-white focus:outline-none focus:ring-primary"
+            type="text"
+            id="nombre"
+            name="nombre"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-white mb-1" htmlFor="correo">
+            Correo electrónico
+          </label>
+          <input
+            className="w-full px-3 py-2 rounded-md bg-darkSecundary text-white focus:outline-none focus:ring-primary"
+            type="email"
+            id="correo"
+            name="correo"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-white mb-1" htmlFor="asunto">
+            Asunto
+          </label>
+          <input
+            className="w-full px-3 py-2 rounded-md bg-darkSecundary text-white focus:outline-none focus:ring-primary"
+            type="text"
+            id="asunto"
+            name="asunto"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-white mb-1" htmlFor="mensaje">
+            Mensaje
+          </label>
+          <textarea
+            className="w-full px-3 py-2 rounded-md bg-darkSecundary text-white focus:outline-none focus:ring-primary"
+            id="mensaje"
+            name="mensaje"
+            rows="4"
+            required
+          ></textarea>
+        </div>
+        <button
+          className="bg-enfasis text-white px-4 py-2 rounded-md hover:bg-primary transition-colors duration-300"
+          type="submit"
+        >
+          Enviar
+        </button>
       </div>
-
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-text">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          {...register('email', { required: true })}
-          className={`w-full px-3 py-2 border rounded-md ${errors.email ? 'border-red-500' : 'border-gray-300'
-            }`}
-        />
-        {errors.email && (
-          <span className="text-red-500">Este campo es requerido</span>
-        )}
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="asunto" className="block text-text">
-          Asunto
-        </label>
-        <input
-          type="text"
-          id="asunto"
-          {...register('asunto', { required: true })}
-          className={`w-full px-3 py-2 border rounded-md ${errors.asunto ? 'border-red-500' : 'border-gray-300'
-            }`}
-        />
-        {errors.asunto && (
-          <span className="text-red-500">Este campo es requerido</span>
-        )}
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="mensaje" className="block text-text">
-          Mensaje
-        </label>
-        <textarea
-          id="mensaje"
-          {...register('mensaje', { required: true })}
-          className={`w-full px-3 py-2 border rounded-md ${errors.mensaje ? 'border-red-500' : 'border-gray-300'
-            }`}
-        />
-        {errors.mensaje && (
-          <span className="text-red-500">Este campo es requerido</span>
-        )}
-      </div>
-
-      <button
-        type="submit"
-        className="bg-primary hover:bg-enfasis text-white px-4 py-2 rounded-md"
-      >
-        Enviar
-      </button>
     </form>
   );
 };
 
-export default Contact;
+
+export default Contact
